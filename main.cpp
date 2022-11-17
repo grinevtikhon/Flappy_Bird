e65976fa08ed0f0a76df6eb0d7ecca86d060fda6
grinevtikhon <grinev.tikhon@gmail.com>
2022-11-17 21:04:04 +0300

    fix+update

--- main.cpp
+++ main.cpp
@@ -4,19 +4,31 @@
 #include <cmath>
 #include <time.h>
 #include <SFML/Graphics.hpp>
+#include <chrono>
+#include <thread>
 
 #define EXPONENT 2.71828182846
-#define SPEED_LEARNING 0.000001
-#define GRAN_LEARN 0.01
+#define SPEED_LEARNING 0.00001
+#define GRAN_LEARN 1
+#define ZOOM 5
+#define HEIGHT 800
+#define LENGTH 800
+
 
 using namespace std;
+using namespace std::this_thread; // sleep_for, sleep_until
+using namespace std::chrono; // nanoseconds, system_clock, seconds
 
-sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+
+sf::RenderWindow window(sf::VideoMode(LENGTH, HEIGHT), "SFML works!");
 sf::CircleShape shape(1.f);
 
-vector<int> str{ 1, 3, 3, 3, 3, 3,  1 };
-vector<long double> parametr{ 4, 11, 1 };
-vector<long double> ans{ 300, 29, 69 };
+vector<int> str{ 1, 10, 10, 1 };
+vector<long double> parametr{ 1, 5, 10, 30 };
+vector<long double> ans{ 1, 23.9, 5, 30 };
+long double bred = 15;
+int gran_chance = 1;
+int gran_da = 20;
 
 long double fun(long double x)
 {
@@ -73,7 +85,7 @@ struct network {
 		for (int i = 0; i < w.size(); ++i)
 			for (int j = 0; j < w[i].size(); ++j)
 				for (int k = 0; k < w[i][j].size(); ++k)
-					w[i][j][k] = rand() % 11 - 5;
+					w[i][j][k] = rand() % 5 - 2;
 
 		return;
 	}
@@ -81,7 +93,7 @@ struct network {
 	{
 		for (int i = 0; i < bias.size(); ++i)
 			for (int j = 0; j < bias[i].size(); ++j)
-				bias[i][j] = rand() % 5 - 2;
+				bias[i][j] = rand() % 10 - 5;
 
 		return;
 	}
@@ -120,23 +132,29 @@ struct network {
 
 		window.clear();
 
-		shape.setRadius(1);
+		shape.setRadius(2);
 		shape.setFillColor(sf::Color::Green);
 
-		for (long double i = 0; i < 1000; i += 1)
+		for (long double i = 0; i < LENGTH; i += 1)
 		{
-			shape.setPosition(i, 800 - function({ i / 80.f })[0]);
+			long double x = (i - LENGTH / 2) / ZOOM;
+			long double y = function({ x })[0];
+			shape.setPosition(x * ZOOM + LENGTH / 2, HEIGHT / 2 - y * ZOOM);
 			window.draw(shape);
 		}
 
-		shape.setRadius(3);
+		shape.setRadius(3.2f);
 		shape.setFillColor(sf::Color::Red);
 
 		for (int i = 0; i < parametr.size(); ++i)
 		{
-			shape.setPosition(parametr[i] * 80, 800 - ans[i]);
+			long double x = parametr[i];
+			long double y = ans[i];
+			shape.setPosition(x * ZOOM + LENGTH / 2 - shape.getRadius() / 2, HEIGHT / 2 - y * ZOOM - shape.getRadius() / 2);
 			window.draw(shape);
 		}
+
+		//sleep_for(100ms);
 
 		window.display();
 		/*
@@ -181,7 +199,7 @@ struct network {
 
 		return;
 	}
-	void setAnswer(vector<long double> &v)
+	void setAnswer(vector<long double>& v)
 	{
 		if (v.size() != delta.back().size())
 		{
@@ -200,7 +218,7 @@ struct network {
 		return value.back();
 	}
 
-	void calculate_neuron(int &x, int &y)
+	void calculate_neuron(int& x, int& y)
 	{
 		value[x][y] = 0;
 		for (int i = 0; i < value[x - 1].size(); ++i) {
@@ -230,15 +248,16 @@ struct network {
 		return;
 	}
 
-	void update_neuron(int &x, int &y)
+	void update_neuron(int& x, int& y)
 	{
 		if (x == 0)
 			return;
 
 		for (int i = 0; i < w[x - 1].size(); ++i)
 		{
-			delta[x - 1][i] = w[x - 1][i][y] * delta[x][y];
+			delta[x - 1][i] += w[x - 1][i][y] * delta[x][y];
 			w[x - 1][i][y] -= value[x - 1][i] * delta[x][y] * SPEED_LEARNING;
+			//cout << w[x - 1][i][y] << endl;
 
 
 		}
@@ -247,7 +266,7 @@ struct network {
 
 		return;
 	}
-	void update_line(int &x)
+	void update_line(int& x)
 	{
 		for (int i = 0; i < value[x].size(); ++i)
 			update_neuron(x, i);
@@ -266,18 +285,26 @@ struct network {
 	{
 		setInput(v);
 		calculate_network();
+		clear_delta();
 		setAnswer(ans);
 		long long st = 0;
+		// bred = rand() % 200 + 10;
+
 		while (!is_right())
 		{
-			//if (st % 1000 == 0 && st != 0)
-				//cout << "< " << getMistake() << " >" << endl;
-			if (st % 50000 == 0 && st != 0)
-				print_network();
+			//if (st % 2000 == 0 && st != 0)
+				//print_network();
+			//cout << "< " << getMistake() << " >" << endl;
+			//if (st % 50000 == 0 && st != 0)
+				//print_network();
 			++st;
 			update_network();
 			calculate_network();
+			clear_delta();
 			setAnswer(ans);
+			int chance = rand() % 101;
+			if (st >= bred && chance > gran_chance)
+				break;
 		}
 		return st;
 	}
@@ -315,7 +342,7 @@ int main()
 
 
 	cout.setf(ios::fixed);
-	cout.precision(4);
+	cout.precision(10);
 	srand(time(NULL));
 
 	network diana(str);
@@ -327,7 +354,7 @@ int main()
 
 	diana.print_network();
 
-	long long da = 1;
+	int da = 1;
 
 	while (true)
 	{
@@ -338,7 +365,7 @@ int main()
 			buf = diana.learn({ parametr[i] }, { ans[i] });
 			cout << buf << "\t";
 			gr += buf;
-			diana.print_network();
+			//diana.print_network();
 		}
 		cout << endl;
 		if (gr <= parametr.size())
@@ -350,16 +377,21 @@ int main()
 				window.close();
 		}
 		++da;
+		if (da % gran_da == 0) {
+			diana.print_network();
+		}
 	}
 
 	window.clear();
 
 	shape.setRadius(1);
-	shape.setFillColor(sf::Color::Green);
+	shape.setFillColor(sf::Color::Blue);
 
-	for (long double i = 0; i < 1000; i += 1)
+	for (long double i = 0; i < LENGTH; i += 1)
 	{
-		shape.setPosition(i, 800 - diana.function({ i / 80.f })[0]);
+		long double x = (i - LENGTH / 2) / ZOOM;
+		long double y = diana.function({ x })[0];
+		shape.setPosition(x * ZOOM + LENGTH / 2, HEIGHT / 2 - y * ZOOM);
 		window.draw(shape);
 	}
 
@@ -368,7 +400,9 @@ int main()
 
 	for (int i = 0; i < parametr.size(); ++i)
 	{
-		shape.setPosition(parametr[i] * 80, 800 - ans[i]);
+		long double x = parametr[i];
+		long double y = ans[i];
+		shape.setPosition(x * ZOOM + LENGTH / 2 - shape.getRadius() / 2, HEIGHT / 2 - y * ZOOM - shape.getRadius() / 2);
 		window.draw(shape);
 	}
 
@@ -387,6 +421,7 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+		window.display();
 	}
 
 	diana.print_network();
